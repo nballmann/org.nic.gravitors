647c2d0473bb47b65e5c55ff25b63af3cd942448
Nicolas Ballmann <nballmann@live.de>
2013-04-20 13:03:45 +0200

    test commit

---
+++ Gravitors/test/org/nic/gravitors/GravitorsTest.java
@@ -0,0 +1,14 @@
+package org.nic.gravitors;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GravitorsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
